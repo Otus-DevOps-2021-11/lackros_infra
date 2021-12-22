ac95c19373e738839f44382cabd7c273852d4ccb
Aleksand Beresnev From Home Ubuntu <aleks.beresnev@gmail.com>
2021-12-22 23:22:10 +0300

    FIX5 deploy testapp

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
+#!/usr/bin/env bash
+# set -euo pipefail
+# IFS=$'\n\t'
 
 sudo apt update -y
 sudo apt install -y ruby-full ruby-bundler build-essential apt-transport-https ca-certificates
