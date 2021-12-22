eef3b78aaccf67f9c1eb2f37b6e3a71d848a6e80
Aleksand Beresnev From Home Ubuntu <aleks.beresnev@gmail.com>
2021-12-22 22:38:56 +0300

    FIX2 deploy testapp

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y ruby-full ruby-bundler build-essential
+apt-get --assume-yes update
+apt-get --assume-yes install ruby-full ruby-bundler build-essential
+ruby -v
+bundler -v
