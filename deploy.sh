b2c58b602934b9e66bbee670753e4e276c302bf9
Aleksand Beresnev From Home Ubuntu <aleks.beresnev@gmail.com>
2021-12-22 23:41:37 +0300

    FIX10 deploy testapp

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
-set -euo pipefail
-IFS=$'\n\t'
-
-sudo apt install git
-git clone https://github.com/express42/reddit
-cd reddit
-bundle install
+#!/bin/bash
+apt-get --assume-yes update
+apt-get --assume-yes install git
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+ps aux | grep puma
 puma -d
